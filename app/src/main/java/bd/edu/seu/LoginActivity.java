6de906367f155c4102e41d4dc636ddb43f0aa88d
hasan2733 <hasan.tng33@gmail.com>
2026-01-10 19:57:06 +0600

    commit by abid

--- app/src/main/java/bd/edu/seu/LoginActivity.java
+++ app/src/main/java/bd/edu/seu/LoginActivity.java
@@ -71,7 +71,7 @@ public class LoginActivity extends AppCompatActivity {
                             finish();
                         } else {
                             Toast.makeText(LoginActivity.this, "User data not found!", Toast.LENGTH_SHORT).show();
-                         }
+                        }
                     }
 
                     @Override
